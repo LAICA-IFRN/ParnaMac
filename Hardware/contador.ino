0cc8771188dac90f62d6b2846397e171859bdc74
GeraldoAquino <89411588+GeraldoAquino@users.noreply.github.com>
2025-01-20 21:53:24 -0300

    Última versão

--- Hardware/contador.ino
+++ Hardware/contador.ino
@@ -1,7 +1,10 @@
+#include <Wire.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <Ultrasonic.h>
 #include <SimpleTimer.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>
 
 const char* ssid = "Laica-IoT";
 const char* password = "Laica321";
@@ -10,12 +13,18 @@ const int trigPin = 16;
 const int echoPin = 5;
 const int distanciaMax = 75;
 const int distanciaMin = 15;
-//const int tolerancia = 5;
+const int distancia_Max_Saida = 220;
+const int tolerancia = 10;
 
 int contadorPessoas = 0;
 
 Ultrasonic ultrassonico(trigPin, echoPin);
 SimpleTimer timer;
+
+// Definindo o display OLED (I2C)
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 void setup() {
   Serial.begin(9600);
@@ -31,22 +40,60 @@ void setup() {
   Serial.println("\nConnected! IP address: ");
   Serial.println(WiFi.localIP());
 
+  // Inicializa o display OLED
+  Wire.begin(D6, D5); // SDA (D6), SCL (D5)
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {  // Endereço padrão 0x3C
+    Serial.println(F("Erro ao inicializar o display OLED"));
+    while (true);  // Se o display não inicializar, trava
+  }
+  display.clearDisplay();
+  
   // Configura o SimpleTimer para chamar a função requisicao() a cada 1 minuto (60.000 milissegundos)
   timer.setInterval(60000, requisicao);
   coldStart();
 }
 
+bool pessoaNoSensor = false;
+
 void contarPessoas() {
   int distancia = ultrassonico.distanceRead();
-  while (distancia > distanciaMax) {
+
+  display.clearDisplay();
+  display.setTextSize(2);  
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+  display.print("Distancia:");
+  display.setCursor(0, 16); 
+  display.print(distancia);
+  display.print(" cm");
+
+  while (distancia > distancia_Max_Saida) {
     distancia = ultrassonico.distanceRead();
     delay(100); 
-    timer.run();  // Adiciona esta linha para garantir que o SimpleTimer continue a funcionar
+    timer.run();
   }
-  if (distancia < distanciaMax && distancia > distanciaMin) {
+  
+  digitalWrite(2, HIGH);
+  if (distancia < distanciaMax && distancia > distanciaMin && !pessoaNoSensor) {
+    Serial.println("Distancia no range: " + String(distancia));
+    pessoaNoSensor = true;
+  }
+
+  if (distancia > (distanciaMax + tolerancia) && pessoaNoSensor) {
     contadorPessoas++;
     Serial.println("Pessoas: " + String(contadorPessoas));
-    delay(1000); // Pequeno delay para evitar contagens múltiplas da mesma pessoa
+    Serial.println("Distancia saindo do range: " + String(distancia));
+
+    
+    display.setCursor(0, 32); 
+    display.print("Pessoas:");
+    display.setCursor(0, 48); 
+    display.print(contadorPessoas);
+
+    display.display();  
+
+    pessoaNoSensor = false;
+    digitalWrite(2, LOW);   
   }
 }
 
@@ -62,7 +109,7 @@ void requisicao() {
     http.begin(client, "http://laica.ifrn.edu.br/access-ng/log/");
     http.addHeader("Content-Type", "application/json");
 
-    String body = "{\"deviceMac\": \"";
+    String body = "{\"deviceMac\": \"";  
     body += WiFi.macAddress();
     body += "\",\"topic\": \"Contador\", \"type\": \"INFO\",\"message\": \"Pessoas = ";
     body += String(contadorPessoas);
@@ -103,7 +150,7 @@ void coldStart() {
     HTTPClient http;
     http.begin(client, "http://laica.ifrn.edu.br/access-ng/log/");  // HTTP
     http.addHeader("Content-Type", "application/json");
-    String body = "{\"deviceMac\": \"";
+    String body = "{\"deviceMac\": \"";  
     body += WiFi.macAddress();
     body += "\",\"topic\": \"ANGeLUS\", \"type\": \"INFO\",\"message\": \"Cold Start\"}";
     int httpCode = http.POST(body);
